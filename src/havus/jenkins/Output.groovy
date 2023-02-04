7dfa705f9d27545fe996759bd6b4524e6477dadc
Hafidz Mahrus <havus.it@gmail.com>
2023-02-04 21:18:28 +0700

    add new func

--- src/havus/jenkins/Output.groovy
+++ src/havus/jenkins/Output.groovy
@@ -2,6 +2,16 @@ package havus.jenkins;
 
 class Output {
   static def sayHello(String name) {
+    // code dibawah tidak akan diprint di jenkins karena beda environment code
+    // pakai sayHelloWithStep untuk bisa dilihat di jenkins log
+    println("In Library - Start")
     println("Hi, ${name}. Welcome aboard")
+    println("In Library - End")
+  }
+
+  static def sayHelloWithStep(steps, String name) {
+    steps.echo("In Library - Start")
+    steps.echo("Hi, ${name}. Welcome aboard")
+    steps.echo("In Library - End")
   }
 }
